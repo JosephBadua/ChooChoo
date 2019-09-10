f7fec5ce5e7cba79c1092de31ad281d59aeebdc6
JosephBadua <josephbadua1006@gmail.com>
2019-09-10 15:09:12 -0700

    got the clock working just need to append to table

--- assests/javascript/java.js
+++ assests/javascript/java.js
@@ -2,57 +2,90 @@
 // getting the values from the objects, placing them into html elements
 // and finally appending them to the table
 var ref = database.ref()
-ref.on('value', gotData, errData);
+
+// event listener 
+ref.on('value', gotData);
+
+// this function gets called every time there is a change in value 
 function gotData(data) {
-var trainData = (data.val());
-console.log(trainData);
-var keyID = Object.keys(trainData);
-console.log(keyID);
-for (var i = 0; i < keyID.length; i++) {
-var t = keyID[i];
-var trainName = trainData[t].trainName;
-var trainDestination = trainData[t].trainDestination;
-var trainTime = trainData[t].trainTime;
-var trainMinutes = trainData[t].trainMinutes;
-console.log(trainName, trainDestination, trainTime, trainMinutes);
-var newRow = "<tr><td>" + trainName + "</td><td>" + trainDestination + "</td><td>" + trainMinutes + "</td></tr>" 
-$(".table").append(newRow);
+    var trainData = (data.val());
+    console.log(trainData);
+    var keyID = Object.keys(trainData);
+    console.log(keyID);
+    for (var i = 0; i < keyID.length; i++) {
+        var t = keyID[i];
+        var trainName = trainData[t].trainName;
+        var trainDestination = trainData[t].trainDestination;
+        var trainMinutes = trainData[t].trainMinutes;
+        //confusing I know but it works
+        var trainTime = trainData[t].trainTime;
+        console.log(trainTime)
+        console.log(trainName, trainDestination, trainTime, trainMinutes);
+        var newRow = "<tr><td>" + trainName + "</td><td>" + trainDestination + "</td><td>" + trainMinutes + "</td></tr>"
+        $(".table").append(newRow);
+    }
+    ref.off('value', gotData);
 }
-}
-function errData(err) {
-console.log("Error!");
-console.log(err);  
-}
-//
-// Getting the Current Time
-var timeNow = moment().format('hh:mm');
-console.log(timeNow);
-var addMinutes = moment().add(5, 'minutes').format('hh:mm');
-console.log(addMinutes);
-var startTime = (moment("20:00", 'HH:mm').format('hh:mm a'));
-console.log(startTime);
+
+
 
 
 //
-$(document).ready(function() {
-    $("#buttonSubmit").on("click", function() {
+$(document).ready(function () {
+    $("#buttonSubmit").on("click", function () {
         $("<tr>,<td>").empty();
         var train = $("#inputTrain").val().trim();
         var destination = $("#inputDestination").val().trim();
-        var time = $("#inputTime").val().trim();
-        var minutes = $("#inputState").val().trim();
+        var time = $("#inputState").val().trim();
+        var time = time.split(":");
+        console.log(time);
+        var timeOne = time[0];
+        console.log(timeOne);
+        var timeTwo = time[1];
+        console.log(timeTwo);
+        var minutes = $("#inputMinutes").val().trim();
         console.log(train)
         console.log(destination)
         console.log(time)
         console.log(minutes)
-        var newRow = "<tr><td>" + train + "</td><td>" + destination + "</td><td>" + minutes + "</td></tr>" 
+            // MOMENT THAT PULLS THE CURRENT TIME
+
+            var timeNow = moment()
+            console.log(timeNow.format('hh:mm'));
+    
+            //
+    
+            // THIS IS ANOTHER MOMENT THAT INPUTS THE FIRST ARRIVAL TIME
+            var startTime = moment()
+            startTime.set({
+            'hour': timeOne,
+            'minute': timeTwo,
+            });
+            console.log(startTime.format('hh:mm'))
+            timeTwo = parseFloat(timeTwo);
+            minutes = parseFloat(minutes)
+            var nextTime = moment ()
+            nextTime.set({
+            'hour': timeOne,
+            'minute': (timeTwo + minutes),
+            })
+            console.log(nextTime);
+            console.log(nextTime.format('hh:mm'));
+            // 
+            var diff = nextTime.diff(startTime, 'minutes')
+    
+            // 
+            console.log(diff)
+            //
+    
+        var newRow = "<tr><td>" + train + "</td><td>" + destination + "</td><td>" + minutes + "</td></tr>"
         $(".table").append(newRow);
         var trainAdd = database.ref().push();
         trainAdd.set({
-            trainName: train,
-            trainDestination: destination,
-            trainTime: time,
-            trainMinutes: minutes
+        trainName: train,
+        trainDestination: destination,
+        trainTime: time,
+        trainMinutes: minutes
         });
     });
 });
